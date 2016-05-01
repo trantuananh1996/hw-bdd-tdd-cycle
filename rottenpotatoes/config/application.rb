a382b39b35479894bfe186e48ea2c870decee850
Trần Anh <trantuananh1996@gmail.com>
2016-05-02 00:52:23 +0700

    part 2

--- rottenpotatoes/config/application.rb
+++ rottenpotatoes/config/application.rb
@@ -1,15 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-# # Pick the frameworks you want:
-# require "active_record/railtie"
-# require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "active_resource/railtie"
-# require "sprockets/railtie"
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-require "rails/all"
 
-Bundler.require(:default, Rails.env)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Rottenpotatoes
   class Application < Rails::Application
@@ -42,28 +46,9 @@ module Rottenpotatoes
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    #config.assets.enabled = true
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    # Enable escaping HTML in JSON.
-
-    config.active_support.escape_html_entities_in_json = true
-
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    #config.active_record.whitelist_attributes = true
-
-    # Fix for Heroku deploy
-    config.assets.initialize_on_precompile = false
-
   end
 end
